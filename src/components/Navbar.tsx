9b4e9c8efaed0eee329d4ba0d6b24d18470224bd
Krish-Parekh <krishparekh303@gmail.com>
2024-05-23 13:10:34 +0530

    feat(core): implemented logout functionality

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,29 @@
-import React from "react";
+"use client";
+
+import React, { useCallback } from "react";
 import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
+import { useCookies } from "react-cookie";
+import { AccessTokenKey } from "@/constants/strings";
+import { cookieOptions } from "@/constants/cookie";
 
 interface INavbarProps {
   username: string;
 }
 
 export default function Navbar({ username }: INavbarProps) {
+  const router = useRouter();
+  const [{ access_token }, removeCookie] = useCookies([AccessTokenKey]);
+
+  const handleLogout = useCallback(() => {
+    removeCookie(AccessTokenKey, cookieOptions);
+    router.replace("/login");
+  }, [removeCookie, router]);
+
   return (
     <div className="flex items-center justify-between">
       <h1 className="text-3xl">Hello, {username}</h1>
-      <Button>Logout</Button>
+      <Button onClick={handleLogout}>Logout</Button>
     </div>
   );
 }

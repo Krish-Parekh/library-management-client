66c260eadec5a11150e720bc3617ab796f949d4b
Krish-Parekh <krishparekh303@gmail.com>
2024-06-05 21:58:02 +0530

    feat: forgot password ssr deployment

--- src/app/(auth)/forgot-password/page.tsx
+++ src/app/(auth)/forgot-password/page.tsx
@@ -1,5 +1,4 @@
-
-import React from "react";
+import React, { Suspense } from "react";
 import {
   Card,
   CardContent,
@@ -9,20 +8,22 @@ import {
 } from "@/components/ui/card";
 import ForgotPasswordForm from "@/components/forms/ForgotPasswordForm";
 
-export default function ResetPassword() {
+export default function ForgotPassword() {
   return (
-    <div className="h-screen w-screen flex items-center justify-center">
-      <Card className="w-11/12 md:w-1/2">
-        <CardHeader className="border-b-2">
-          <CardTitle>Forgot Password</CardTitle>
-          <CardDescription>
-            Forgot Password link will be sent to regsitered email address
-          </CardDescription>
-        </CardHeader>
-        <CardContent className="p-4">
-          <ForgotPasswordForm />
-        </CardContent>
-      </Card>
-    </div>
+    <Suspense fallback={null}>
+      <div className="h-screen w-screen flex items-center justify-center">
+        <Card className="w-11/12 md:w-1/2">
+          <CardHeader className="border-b-2">
+            <CardTitle>Forgot Password</CardTitle>
+            <CardDescription>
+              Forgot Password link will be sent to regsitered email address
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="p-4">
+            <ForgotPasswordForm />
+          </CardContent>
+        </Card>
+      </div>
+    </Suspense>
   );
 }

2950afbfd946e35ef065cdbb49d4215b2e637b62
Krish-Parekh <krishparekh303@gmail.com>
2024-06-10 13:46:33 +0530

    self delete removed

--- src/components/table/user/columns.tsx
+++ src/components/table/user/columns.tsx
@@ -12,6 +12,8 @@ import { Button } from "@/components/ui/button";
 import { DeleteIcon } from "lucide-react";
 import TrashItem from "../TrashItem";
 import EditItem from "../EditItem";
+import { Cookies } from "react-cookie";
+import { UserIdKey } from "@/constants/strings";
 
 export const userTableColumns: ColumnDef<User>[] = [
   {
@@ -42,9 +44,12 @@ export const userTableColumns: ColumnDef<User>[] = [
     cell: ({ row }) => {
       const id = row.original._id;
       const url = `/user/${id}/`;
+      const userId = new Cookies().get(UserIdKey);
       return (
         <div className="flex gap-x-4 justify-end">
-          <TrashItem url={url} revalidationURL={["/user/"]} />
+          {userId !== id && (
+            <TrashItem url={url} revalidationURL={["/user/"]} />
+          )}
           <EditItem id={id} type="users" />
         </div>
       );
